0bd6a62892bc4009b22e53ea6f526aa4be196b24
fatemeh <82167145+fatemh969@users.noreply.github.com>
2022-02-15 22:51:12 +0330

    edit1

--- js/index.js
+++ js/index.js
@@ -196,9 +196,5 @@ $(document).ready(function () {
   })
   $(".accordion").on("click", ".heading", function() {
 
-    $(this).toggleClass("active").next().slideToggle();
-
-    $(".contents").not($(this).next()).slideUp(300);
-                 
-    $(this).siblings().removeClass("active");})
+    $(this).toggleClass("active").next().slideToggle();})
 });
